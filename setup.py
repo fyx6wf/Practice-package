71642317554c4ebe8cb2b2fb99d6f3db099651c7
James Hopper <fyx6wf@virginia.edu>
2024-07-02 11:10:45 -0400

    classwork

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = 'Test',
+	version = '0.1',
+	description = 'A simple demonstration',
+	url = 'https://github.com/fyx6wf/Practice-package',
+	author = 'James Hopper',
+	author_email = 'fyx6wf@virginia.edu',
+	license = 'MIT',
+	packages = ['mypackage'])
